be33c0338e5b2bcf00d595f046a1155b30ce54e1
Aarushi Seth <aarushi.seth9@gmail.com>
2022-05-25 23:52:59 -0500

    Finished HW4

--- homeworks/hw04/your_task/js/scripts.js
+++ homeworks/hw04/your_task/js/scripts.js
@@ -27,25 +27,29 @@ const getTracks = (term) => {
 
         console.log(firstFive[0]);
 
-        //   covnvert to html
+        //   convert to html
         let html = firstFive.map(track2Html);
 
         //  plug it back to the index.html file
         document.querySelector("#tracks").innerHTML = html;
+
       } else {
         let html = "<p>No tracks found that match your search criteria. </p>";
         document.querySelector("#tracks").innerHTML = html;
       }
-    });
+    }
+    )
+    .then(() =>  playTrack());
+    
 };
 
 const track2Html = (track) => {
   return `
     <button class="track-item preview" data-preview-track=${track.preview_url} onclick="handleTrackClick(event);">
-            <img src=${track.album.image_url}>
-            <i class="fas play-track fa-play" aria-hidden="true"></i>
+            <img src=${track.album.image_url} alt = "Album cover for ${track.album.name}">
+            <i class="fas play-track fa-play" aria-label="Play track ${track.name}"></i>
             <div class="label">
-                <h2>${track.album.name}</h2>
+                <h2>${track.name}</h2>
                 <p>
                  ${track.artist.name}
                 </p>
@@ -54,14 +58,39 @@ const track2Html = (track) => {
     `;
 };
 
-getTracks("pink gloyd");
-
 const getAlbums = (term) => {
-  console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+  let url = `https://www.apitutor.org/spotify/simple/v1/search?type=album&q=${term}`;
+  console.log('Fetching albums now');
+  fetch(url)
+    .then((response) => response.json())
+    .then((albums) => {
+      console.log(albums);
+      document.querySelector("#albums").innerHTML = "";
+      if (albums.length > 0) {
+        for (const album of albums) {
+          document.querySelector("#albums").innerHTML += `
+          <section class="album-card" id=${album.id}>
+          <div>
+              <img src=${album.image_url} alt = "Album cover for ${album.name}">
+              <h2>${album.name}</h2>
+              <div class="footer">
+                  <a href=${album.spotify_url} target="_blank">
+                      view on spotify
+                  </a>
+              </div>
+          </div>
+        </section>
+          `;
+        }
+
+      } else {
+        let html = "<p>No albums found that match your search criteria. </p>";
+        document.querySelector("#albums").innerHTML = html;
+      }
+    });
+  
 };
+
 
 const getArtist = (term) => {
   let url = `https://www.apitutor.org/spotify/simple/v1/search?type=artist&q=${term}`;
@@ -89,7 +118,7 @@ const artist2Html = (artist) => {
   return `
     <section class="artist-card" id=${artist.id}>
         <div>
-            <img src=${artist.image_url}>
+            <img src=${artist.image_url} alt = "Picture of ${artist.name}">
             <h2>${artist.name}</h2>
             <div class="footer">
                 <a href=${artist.spotify_url} target="_blank">
@@ -100,11 +129,30 @@ const artist2Html = (artist) => {
     </section>  `;
 };
 
-getArtist("Lady gaga");
 
 const handleTrackClick = (ev) => {
   const previewUrl = ev.currentTarget.getAttribute("data-preview-track");
   console.log(previewUrl);
+};
+
+const playTrack = () => {
+  const tracks = document
+    .getElementById("tracks")
+    .querySelectorAll(".track-item");
+  console.log(tracks.length);
+  tracks.forEach((track) => {
+    track.addEventListener("click", (event) => {
+      const trackPreviewContainer = document
+        .querySelector("footer")
+        .getElementsByClassName("track-item")[0];
+      trackPreviewContainer.innerHTML = `${event.currentTarget.innerHTML}`;
+
+      const songUrl = event.currentTarget.getAttribute("data-preview-track");
+      audioPlayer.setAudioFile(songUrl);
+
+      audioPlayer.play();
+    });
+  });
 };
 
 document.querySelector("#search").onkeyup = (ev) => {
@@ -115,3 +163,7 @@ document.querySelector("#search").onkeyup = (ev) => {
     search();
   }
 };
+
+getTracks("Harry Styles");
+getAlbums("Harry Styles");
+getArtist("Harry Styles");
